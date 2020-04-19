4e89217d7e8a8db280139c41c64cc72605d2c92a
nico-develop <contact@nico-develop.fr>
2020-04-19 19:09:03 +0200

    Issue #35 - Mise en place des bases du bandeau de cookies

---
+++ components/CookieNotify.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-alert
+    v-model="show"
+    color="blue-grey darken-4"
+    close-text="Fermer"
+    dark
+    dismissible
+    tile
+    class="mb-0"
+  >
+    <small>Nous utilisons des cookies pour nous permettre de mieux comprendre comment le site est utilisé. En
+      continuant à utiliser ce site, vous acceptez cette politique.</small>
+  </v-alert>
+</template>
+
+<script>
+    export default {
+        name: "cookies-notify",
+        data: () => ({
+            show: true
+        })
+    }
+</script>
