234baa4e9dd5d377a6a3badc76c60400584950c4
nico-develop <contact@nico-develop.fr>
2020-04-20 09:18:33 +0200

    Modification de la commande pour intégrer le navigateur

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,6 +4,9 @@ module.exports = {
     port: 3000,
     launchTimeout: 30000
   },
-  browser: 'chromium',
-  browserContext: 'default'
+  launch: {
+    headless: process.env.CI === "true",
+    ignoreDefaultArgs: ["--disable-extensions"],
+    args: ["--no-sandbox"]
+  }
 }
