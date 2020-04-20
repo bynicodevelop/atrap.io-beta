c098b3abdf85a248cf06462d15dc7449093630e3
nico-develop <contact@nico-develop.fr>
2020-04-20 10:36:55 +0200

    Changement du headless

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -5,7 +5,7 @@ module.exports = {
     launchTimeout: 30000
   },
   launch: {
-    headless: process.env.CI === true,
+    headless: true,
     ignoreDefaultArgs: ["--disable-extensions"],
     args: ["--no-sandbox", "--disable-setuid-sandbox"]
   },
