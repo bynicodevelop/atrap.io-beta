10c52fccdd31a3ecb77134b99e9f65e5a964ef15
nico-develop <contact@nico-develop.fr>
2020-04-20 18:39:28 +0200

    Issue #7 - Création du système de notification

---
+++ components/Notify.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-snackbar v-model="snackbar">
+    {{ text }}
+    <v-btn text :timeout="timeout" @click="snackbar = false">
+      Close
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: "Notify",
+  props: {
+    text: {
+      type: String,
+      default: null,
+    },
+    timeout: {
+      type: Number,
+      default: 4000,
+    },
+    snackbar: {
+      type: Boolean,
+      default: true,
+    },
+  },
+}
+</script>
