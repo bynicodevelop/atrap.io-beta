177557bce689d5fea1822b16d21d1b60c959139d
nico-develop <contact@nico-develop.fr>
2020-04-30 10:53:22 +0200

    Mise à jour de Eslint pour intégrer cypress et du code html

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,11 +14,13 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // préférer utiliser `plugin:vue/strongly-recommended` ou `plugin:vue/recommended` pour des règles stictes.
     'plugin:vue/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'plugin:cypress/recommended',
   ],
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'vue',
+    'cypress'
   ],
   // add your custom rules here
   rules: {
@@ -27,6 +29,13 @@ module.exports = {
     'vue/max-attributes-per-line': 'off',
     'prettier/prettier': ['error', { 'semi': false }],
     'no-unused-vars': ["error", { "argsIgnorePattern": "config|ctx|commit" }],
-    'indent': ["error", 2]
+    'indent': ["error", 2],
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "never",
+        "component": "any"
+      }
+    }]
   }
 }
