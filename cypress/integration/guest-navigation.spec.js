b5e72f4f1665a48bf7dbec713a60b0b828750354
nico-develop <contact@nico-develop.fr>
2020-05-01 10:04:10 +0200

    Refactoring de code dans les tests fonctionnelles

--- cypress/integration/guest-navigation.spec.js
+++ cypress/integration/guest-navigation.spec.js
@@ -1,39 +1,29 @@
+const gotTo = (visit, selector, expected) => {
+  cy.visit(visit)
+
+  if(selector !== null) {
+    cy.get(selector).click()
+  }
+
+  cy.location().should((loc) => {
+    expect(loc.href).to.eq(expected)
+  })
+}
+
 describe("Guest Navigation", function () {
   it("Should automatically redirected to Signin", function () {
-    cy.visit("http://localhost:3000")
-
-    cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:3000/auth/signin")
-    })
+    gotTo("http://localhost:3000", null, "http://localhost:3000/auth/signin")
   })
 
   it("Should navigate to go Sign up page", function () {
-    cy.visit("http://localhost:3000/auth/signin")
-
-    cy.get(".v-toolbar__content > .v-btn").click()
-
-    cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:3000/auth/signup")
-    })
+    gotTo("http://localhost:3000/auth/signin", ".v-toolbar__content > .v-btn", "http://localhost:3000/auth/signup")
   })
 
   it("Should navigate to go Sign in page", function () {
-    cy.visit("http://localhost:3000/auth/signup")
-
-    cy.get(".v-toolbar__content > .v-btn").click()
-
-    cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:3000/auth/signin")
-    })
+    gotTo("http://localhost:3000/auth/signup", ".v-toolbar__content > .v-btn", "http://localhost:3000/auth/signin")
   })
 
   it("Should navigate to go Reset password page", function () {
-    cy.visit("http://localhost:3000/auth/signin")
-
-    cy.get(".flex > a").click()
-
-    cy.location().should((loc) => {
-      expect(loc.href).to.eq("http://localhost:3000/auth/reset")
-    })
+    gotTo("http://localhost:3000/auth/signin", ".flex > a", "http://localhost:3000/auth/reset")
   })
 })
